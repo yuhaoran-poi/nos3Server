f5a505968363538b1d7dc36cd27709a0371b7801
yuhaoran <357799943@qq.com>
2025-11-14 16:24:51 +0800

    fix_conf

--- server/static/table/BootyBackpackExpansion.lua
+++ server/static/table/BootyBackpackExpansion.lua
@@ -3,7 +3,7 @@
 ---@field public booty_backpack_grids integer @战利品背包格子数量
 ---@field public booty_backpack_cost table @战利品背包扩充消耗
 return {
-[1] = { id=1,booty_backpack_grids=20,booty_backpack_cost={} },
+[1] = { id=1,booty_backpack_grids=60,booty_backpack_cost={} },
 [2] = { id=2,booty_backpack_grids=0,booty_backpack_cost={} },
 [3] = { id=3,booty_backpack_grids=0,booty_backpack_cost={} },
 [4] = { id=4,booty_backpack_grids=0,booty_backpack_cost={} },
