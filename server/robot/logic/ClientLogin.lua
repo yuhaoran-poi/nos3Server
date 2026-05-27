bed9964f4e42b5c03ef37454dc3c69a38a144931
yipeng <247561541@qq.com>
2026-05-27 12:43:05 +0800

    pb version修改

--- server/robot/logic/ClientLogin.lua
+++ server/robot/logic/ClientLogin.lua
@@ -15,7 +15,7 @@ function Client:login()
     local login_msg = {
         login_data = {
             authkey = self.username .. self.index,
-            pb_version = "1036032865",
+            pb_version = "2357358251",
         },
         is_register = false,
         password = moon.md5("123456"),
