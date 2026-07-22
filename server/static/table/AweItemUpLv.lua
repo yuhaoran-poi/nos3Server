a609f85edc23110e527f53438c1e513c5b6b87cf
yuhaoran <357799943@qq.com>
2026-07-22 19:48:16 +0800

    conf

--- server/static/table/AweItemUpLv.lua
+++ server/static/table/AweItemUpLv.lua
@@ -4,19 +4,19 @@
 ---@field public condition integer @升级条件：赛季通行证等级 达到该等级后才能升级到该等级
 ---@field public max_star integer @该等级允许提升的最大星级
 return {
-[1] = { id=1,cost={[1]=10},condition=0,max_star=1 },
-[2] = { id=2,cost={[1]=11},condition=0,max_star=1 },
-[3] = { id=3,cost={[1]=12},condition=0,max_star=1 },
-[4] = { id=4,cost={[1]=13},condition=10,max_star=2 },
-[5] = { id=5,cost={[1]=14},condition=10,max_star=2 },
-[6] = { id=6,cost={[1]=15},condition=10,max_star=2 },
-[7] = { id=7,cost={[1]=16},condition=20,max_star=3 },
-[8] = { id=8,cost={[1]=17},condition=20,max_star=3 },
-[9] = { id=9,cost={[1]=18},condition=20,max_star=3 },
-[10] = { id=10,cost={[1]=19},condition=30,max_star=4 },
-[11] = { id=11,cost={[1]=20},condition=30,max_star=4 },
-[12] = { id=12,cost={[1]=21},condition=30,max_star=4 },
-[13] = { id=13,cost={[1]=22},condition=40,max_star=5 },
-[14] = { id=14,cost={[1]=23},condition=40,max_star=5 },
-[15] = { id=15,cost={[1]=24},condition=40,max_star=5 }
+[1] = { id=1,cost={[1]=100000},condition=0,max_star=1 },
+[2] = { id=2,cost={[1]=200000},condition=0,max_star=1 },
+[3] = { id=3,cost={[1]=300000},condition=0,max_star=1 },
+[4] = { id=4,cost={[1]=400000},condition=10,max_star=2 },
+[5] = { id=5,cost={[1]=500000},condition=10,max_star=2 },
+[6] = { id=6,cost={[1]=600000},condition=10,max_star=2 },
+[7] = { id=7,cost={[1]=700000},condition=20,max_star=3 },
+[8] = { id=8,cost={[1]=800000},condition=20,max_star=3 },
+[9] = { id=9,cost={[1]=900000},condition=20,max_star=3 },
+[10] = { id=10,cost={[1]=1000000},condition=30,max_star=4 },
+[11] = { id=11,cost={[1]=1100000},condition=30,max_star=4 },
+[12] = { id=12,cost={[1]=1200000},condition=30,max_star=4 },
+[13] = { id=13,cost={[1]=1300000},condition=40,max_star=5 },
+[14] = { id=14,cost={[1]=1400000},condition=40,max_star=5 },
+[15] = { id=15,cost={[1]=1500000},condition=40,max_star=5 }
 }
--- server/static/table/CommonConfig.lua
+++ server/static/table/CommonConfig.lua
@@ -27,7 +27,7 @@ return {
 [21] = { id=21,name="Initialhuman_LV",value=1,items={} },
 [22] = { id=22,name="Initialitem_LV",value=1,items={} },
 [23] = { id=23,name="ChangeGhostNameCost",value=0,items={[1]=1000} },
-[24] = { id=24,name="MaintenanceCost",value=1,items={[1]=1000} },
+[24] = { id=24,name="MaintenanceCost",value=1,items={[1]=100} },
 [25] = { id=25,name="DungeonAwardEmailId",value=2000021,items={} },
 [26] = { id=26,name="UpStarAdditionRate",value=500,items={} },
 [27] = { id=27,name="TreasureBoxEmail",value=2000020,items={} },
