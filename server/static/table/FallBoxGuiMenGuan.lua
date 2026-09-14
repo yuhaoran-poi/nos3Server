b0ee6b8b03288ab01a347bd3233833411d8dbaf1
yuhaoran <357799943@qq.com>
2026-09-14 20:24:12 +0800

    conf

---
+++ server/static/table/FallBoxGuiMenGuan.lua
@@ -0,0 +1,10 @@
+---@class FallBoxGuiMenGuan_cfg
+---@field public id integer @唯一ID(必须为第一列且名称为id，类型为INT) 章节*100+难度
+---@field public item_info table @掉落的资源：
+return {
+[100101] = { id=100101,item_info={[1120005]=5} },
+[100102] = { id=100102,item_info={[1120006]=5} },
+[100103] = { id=100103,item_info={[1120007]=5} },
+[100104] = { id=100104,item_info={[1120008]=5} },
+[100105] = { id=100105,item_info={[1120043]=5} }
+}
